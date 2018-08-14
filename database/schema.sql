c6ee4db3dcc3b88cff486c77b2f33c68532e8907
kcooler <kcooler@gmail.com>
2018-08-14 11:23:39 -0400

    updates user dao

--- database/schema.sql
+++ database/schema.sql
@@ -16,8 +16,8 @@ CREATE TABLE app_user (
   id SERIAL PRIMARY KEY,
   user_name varchar(32) NOT NULL UNIQUE,
   password varchar(32) NOT NULL,
-  role varchar(32),
-  salt varchar(255) NOT NULL
+  role varchar(32) DEFAULT 'client',
+  salt varchar(255)
 );
 
 CREATE TABLE coaches (
