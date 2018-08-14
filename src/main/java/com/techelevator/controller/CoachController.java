673f41a6fb3cb2ef4068b50b1359b9f33c915f1a
bmulh <mulh.brian@gmail.com>
2018-08-14 10:04:15 -0400

    connect coach page to database

--- src/main/java/com/techelevator/controller/CoachController.java
+++ src/main/java/com/techelevator/controller/CoachController.java
@@ -2,23 +2,30 @@ package com.techelevator.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import com.techelevator.model.Coach;
+import com.techelevator.model.CoachDAO;
 import com.techelevator.model.UserDAO;
 
 @Controller
 public class CoachController {
 	
-	private UserDAO userDAO;
-
+	private CoachDAO coachDAO;
+	
+	
 	@Autowired
-	public CoachController(UserDAO userDAO) {
-		this.userDAO = userDAO;
+	public CoachController(CoachDAO coachDAO) {
+		this.coachDAO = coachDAO;
 	}
 	
 	@RequestMapping(path="/coach", method=RequestMethod.GET)
-	public String displayCoach() {
+	public String displayCoach(@RequestParam long coachId, ModelMap map) {
+		Coach coach = coachDAO.getCoachById(coachId);
+		map.addAttribute("coach", coach);
 		return "coach";
 	}
 	
