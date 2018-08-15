7c05c7821048c68575dc96355728828a669b40e4
kdaniszewski <kerry@kerda.com>
2018-08-14 21:26:49 -0400

    Created the Review object/DAO.

--- src/main/java/com/techelevator/controller/CoachController.java
+++ src/main/java/com/techelevator/controller/CoachController.java
@@ -1,15 +1,9 @@
 package com.techelevator.controller;
 
-import java.io.IOException;
-
-import javax.servlet.http.HttpServletResponse;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
