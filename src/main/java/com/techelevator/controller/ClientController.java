ea4897a2685b5019b044be55b033ebb5755ff1e5
bmulh <mulh.brian@gmail.com>
2018-08-15 13:43:45 -0400

    updating client controller

--- src/main/java/com/techelevator/controller/ClientController.java
+++ src/main/java/com/techelevator/controller/ClientController.java
@@ -2,25 +2,30 @@ package com.techelevator.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.techelevator.model.DAOs.AvailabilityDAO;
+import com.techelevator.model.DAOs.ClientDAO;
 import com.techelevator.model.DAOs.CoachDAO;
 import com.techelevator.model.DAOs.UserDAO;
+import com.techelevator.model.Objects.Client;
 import com.techelevator.model.Objects.Coach;
 
 @Controller
 public class ClientController {
 	
 	private CoachDAO coachDAO;
+	private ClientDAO clientDAO;
 //	private UserDAO userDAO;
 //	private AvailabilityDAO availDAO;
 	
 	@Autowired
-	public ClientController(CoachDAO coachDAO, UserDAO userDAO, AvailabilityDAO availDAO) {
+	public ClientController(CoachDAO coachDAO, ClientDAO clientDAO, UserDAO userDAO, AvailabilityDAO availDAO) {
+		this.clientDAO = clientDAO;
 		this.coachDAO = coachDAO;
 //		this.userDAO = userDAO;
 //		this.availDAO = availDAO;
@@ -33,8 +38,22 @@ public class ClientController {
 	}
 	
 	@RequestMapping(path="/client", method=RequestMethod.GET)
-	public String displayClientProfile() {
-		return "client";
+	public String displayClientProfile(@RequestParam(required=false) Long clientId, ModelMap map, Model model) {
+		Client client;
+		
+		if(model.containsAttribute("clientId")) {
+			long id = (long) model.asMap().get("clientId");
+			client = clientDAO.getClientById(id);
+			map.addAttribute("client", client);
+		} else if(clientId == null) {
+			return "redirect:/";
+		} else {
+			client = clientDAO.getClientById(clientId);
+			map.addAttribute("client", client);
+		} if(client != null) {
+			return "client";
+		}
+		return "redirect:/";
 	}
 
 	@RequestMapping(path="/newMessage", method=RequestMethod.GET)
